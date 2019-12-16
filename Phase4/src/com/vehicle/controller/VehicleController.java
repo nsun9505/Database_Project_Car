b3251816a995dbe8dd95ebd93f1d8e7196a29f8b
pdj16 <pdj16@DESKTOP-K4358LD>
2019-12-17 05:00:57 +0900

    매물등록완료

--- Phase4/src/com/vehicle/controller/VehicleController.java
+++ Phase4/src/com/vehicle/controller/VehicleController.java
@@ -2,6 +2,8 @@ package com.vehicle.controller;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.sql.Date;
+import java.util.*;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -12,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.account.vo.AccountVO;
+import com.vehicle.dao.VehicleDAO;
 import com.vehicle.service.VehicleService;
 
 /**
@@ -21,6 +25,11 @@ import com.vehicle.service.VehicleService;
 @WebServlet("/vehicle/*")
 public class VehicleController extends HttpServlet {
 	VehicleService vehicleService;
+
+	public VehicleController() {
+		// TODO Auto-generated constructor stub
+	}
+
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
@@ -29,43 +38,126 @@ public class VehicleController extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doHandle(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doHandle(request, response);
 	}
 
-	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
-		String nextPage ="";
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response)
+			throws UnsupportedEncodingException {
+		String nextPage = "";
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
 		String action = request.getPathInfo();
 		System.out.println(action);
 		try {
-			if(action.equals("/list.do")) {
+			if (action.equals("/list.do")) {
 				nextPage = "/index.jsp";
-			} else if(action.equals("/addVehicle.do")) {
+			} else if (action.equals("/addVehicle.do")) {
 				// 매물 등록 작업
-				String make = (String)request.getParameter("make");
-				System.out.println(make);
-				vehicleService.addVehicle();
-				nextPage = "/index.jsp";
+				String car_number = (String) request.getParameter("car_number");
+				car_number = vehicleService.checkCarnumber(car_number);
+				if (car_number == null) {
+					nextPage = "/Vehicle/addVehicleFail.jsp";
+				} else {
+					String make = (String) request.getParameter("make");
+					String model = (String) request.getParameter("model_name");
+		
+					String detailed_model = (String) request.getParameter("detailed_model_name");
+					int engine = Integer.parseInt((String) request.getParameter("engine_displacement"));
+					String year = (String) request.getParameter("model_year");
+					String month = (String) request.getParameter("model_month");
+					String fuel = (String) request.getParameter("fuel");
+					String color = (String) request.getParameter("color");
+					String transmission = (String) request.getParameter("transmission");
+					int price = Integer.parseInt((String) request.getParameter("price"));
+					int mileage = Integer.parseInt((String) request.getParameter("mileage"));
+					String location = (String) request.getParameter("location");
+					String sellerId;
+
+					HttpSession session = request.getSession();
+					AccountVO user = (AccountVO) session.getAttribute("userInfo");
+					sellerId = user.getId();
+
+					if(Integer.parseInt(month)<10)
+						month="0"+month;
+					String model_year = year+"-"+month+"-"+"01";
+					
+					System.out.println(detailed_model + model_year+ price+ mileage+ location+ fuel+ color+
+							engine+ transmission+ car_number+ sellerId);
+					vehicleService.addVehicle(detailed_model, model_year, price, mileage, location, fuel, color,
+							engine, transmission, car_number, sellerId);
+					nextPage = "/index.jsp";
+				}
+			} else if (action.equals("/getMake.do")) {
+				ArrayList<String> makelist = vehicleService.getMakeList();
+				HttpSession session = request.getSession();
+
+				session.setAttribute("makelist", makelist);
+
+				nextPage = "/Vehicle/addVehicle.jsp";
+			} else if (action.equals("/getModel.do")) {
+				String make = (String) request.getParameter("make");
+				ArrayList<String> modelList = vehicleService.getModelList(make);
+				HttpSession session = request.getSession();
+
+				session.setAttribute("modelList", modelList);
+				session.setAttribute("selected_make", make);
+
+				nextPage = "/Vehicle/addVehicle.jsp";
+			} else if (action.equals("/getDetailed.do")) {
+				String model = (String) request.getParameter("model_name");
+				ArrayList<String> detaileList = vehicleService.getDetailelList(model);
+				HttpSession session = request.getSession();
+
+				session.setAttribute("detaileList", detaileList);
+				session.setAttribute("selected_model", model);
+
+				nextPage = "/Vehicle/addVehicle.jsp";
+			} else if (action.equals("/getEngine.do")) {
+				String detaile = (String) request.getParameter("detailed_model_name");
+				ArrayList<String> EngineList = vehicleService.getEngineList(detaile);
+				HttpSession session = request.getSession();
+
+				session.setAttribute("EngineList", EngineList);
+				session.setAttribute("selected_detaile", detaile);
+
+				nextPage = "/Vehicle/addVehicle.jsp";
+			} else if (action.equals("/getElse.do")) {
+				String engine = (String) request.getParameter("engine_displacement");
+				HttpSession session = request.getSession();
+				ArrayList<String> fuelList = vehicleService.getFuelList();
+				ArrayList<String> transList = vehicleService.getTransList();
+				ArrayList<String> colorList = vehicleService.getColorList();
+
+				session.setAttribute("selected_engine", engine);
+				session.setAttribute("fuelList", fuelList);
+				session.setAttribute("colorList", colorList);
+				session.setAttribute("transList", transList);
+
+				nextPage = "/Vehicle/addVehicle.jsp";
+
 			}
 			RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
 			dispatcher.forward(request, response);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
--- Phase4/src/com/vehicle/dao/VehicleDAO.java
+++ Phase4/src/com/vehicle/dao/VehicleDAO.java
@@ -1,5 +1,258 @@
 package com.vehicle.dao;
 
-public class VehicleDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import com.vehicle.vo.VehicleVO;
+
+public class VehicleDAO {
+	
+	private static final String getBasicVehicleInfoQuery = "select registration_number, make, detailed_model_name, model_year, price, mileage, location, fuel, color from ALL_VEHICLE_INFO where registration_number not in (select registration_number from order_list) order by registration_number desc";
+	private static final String getTableColumnNamesQuery = "select cname from col where tname=?";
+	private static final String getMakeListQuery = "select * from make";
+	private static final String searchQuery = "select ? from selling_car";
+	private static final String url = "jdbc:oracle:thin:@localhost:1600:xe";
+	private static final String user = "knu";
+	private static final String pw = "comp322";
+	private static final String buyVehicleQuery = "insert into order_list values(?, ?, ?, sysdate)";
+	private static final String addVehicleInsertQuery="";
+	private static final String regExpCarNum = "^[0-9]{2}[A-Z]{2}[0-9]{4}$";
+	private PreparedStatement pstmt;
+	private Connection conn;
+	private DataSource dataSrc;
+	
+	public VehicleDAO() {
+		try {
+			Context context = new InitialContext();
+			dataSrc = (DataSource) context.lookup("java:comp/env/jdbc/Oracle");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean insertVehicle(VehicleVO newVehicle){
+		try {
+			
+			String addVehicleInsertQuery = "insert into vehicle values('" +  newVehicle.getCar_number() + "',registration_seq.nextVal,"
+					+ newVehicle.getPrice() + "," + newVehicle.getMileage() + "," + "TO_DATE('" + newVehicle.getModel_year() + "','yyyy-mm-dd')" + ",'" + newVehicle.getLocation() + "',"
+					+ newVehicle.getEngine_displacement() + ",'" + newVehicle.getDetailed_model_name() + "','" + newVehicle.getColor() + "','" + newVehicle.getTransmission() + "','"
+					+ newVehicle.getFuel() + "','" + newVehicle.getSellerId() + "')";
+			
+			conn = dataSrc.getConnection();
+			pstmt = conn.prepareStatement(addVehicleInsertQuery);
+			
+			 /*pstmt.setString(1, newVehicle.getCar_number());
+			 pstmt.setString(2,"registration_seq.nextVal"); 
+			 pstmt.setInt(3, newVehicle.getPrice()); 
+			 pstmt.setInt(4,newVehicle.getMileage());
+			 pstmt.setDate(5, java.sql.Date.valueOf(newVehicle.getModel_year()));
+			 pstmt.setString(6, newVehicle.getLocation()); 
+			 pstmt.setInt(7,newVehicle.getEngine_displacement()); 
+			 pstmt.setString(8,newVehicle.getDetailed_model_name());
+			 pstmt.setString(9, newVehicle.getColor()); 
+			 pstmt.setString(10, newVehicle.getTransmission());
+			 pstmt.setString(11, newVehicle.getFuel());
+			 pstmt.setString(12, newVehicle.getSellerId());*/
+			 
+			 
+
+			int res = pstmt.executeUpdate();
+			if (res == 1) {
+				conn.commit();
+				return true;
+			}
+		} catch (SQLException e) {
+			System.err.println("sql error : " + e.getMessage());
+		}
+		return false;
+	}
+	
+	public ArrayList<String> getMake() {
+
+		ArrayList<String> list = new ArrayList<String>();
+		ResultSet rs = null;
+
+		try {
+			conn = dataSrc.getConnection();
+			pstmt = conn.prepareStatement("select make from make");
+			rs = pstmt.executeQuery();
+
+			while (rs.next())
+				list.add(rs.getString(1));
+
+		} catch (SQLException e) {
+			System.err.println("[getMake] sql error " + e.getMessage());
+		} 
+
+		return list;
+	}
+
+	public ArrayList<String> getModel(String make) {
+		ArrayList<String> list = new ArrayList<String>();
+		ResultSet rs = null;
+
+		try {
+			conn = dataSrc.getConnection();
+			pstmt = conn.prepareStatement("select model_name from model where make = '"+make+"'");
+			rs = pstmt.executeQuery();
+
+			while (rs.next())
+				list.add(rs.getString(1));
+
+		} catch (SQLException e) {
+			System.err.println("[getModel] sql error " + e.getMessage());
+		} 
+
+		return list;
+	}
+
+	public ArrayList<String> getDetaile(String model) {
+		ArrayList<String> list = new ArrayList<String>();
+		ResultSet rs = null;
+
+		try {
+			conn = dataSrc.getConnection();
+			pstmt = conn.prepareStatement("select detailed_model_name from detailed_model where model_name = '"+model+"'");
+			rs = pstmt.executeQuery();
+
+			while (rs.next())
+				list.add(rs.getString(1));
+
+		} catch (SQLException e) {
+			System.err.println("[getDetaile] sql error " + e.getMessage());
+		} 
+
+		return list;
+	}
+	
+	public ArrayList<String> getEngine(String detaile) {
+		ArrayList<String> list = new ArrayList<String>();
+		ResultSet rs = null;
+
+		try {
+			conn = dataSrc.getConnection();
+			pstmt = conn.prepareStatement("select engine_displacement from engine_displacement where detailed_model_name = '"+detaile+"'");
+			rs = pstmt.executeQuery();
+
+			while (rs.next())
+				list.add(rs.getString(1));
+
+		} catch (SQLException e) {
+			System.err.println("[getEngine] sql error " + e.getMessage());
+		} 
+
+		return list;
+	}
+	
+	public ArrayList<String> getFuel() {
+		ArrayList<String> list = new ArrayList<String>();
+		ResultSet rs = null;
+
+		try {
+			conn = dataSrc.getConnection();
+			pstmt = conn.prepareStatement("select fuel from fuel");
+			rs = pstmt.executeQuery();
+
+			while (rs.next())
+				list.add(rs.getString(1));
+
+		} catch (SQLException e) {
+			System.err.println("[getFuel] sql error " + e.getMessage());
+		} 
+
+		return list;
+	}
+	
+	public ArrayList<String> getColor() {
+		ArrayList<String> list = new ArrayList<String>();
+		ResultSet rs = null;
+
+		try {
+			conn = dataSrc.getConnection();
+			pstmt = conn.prepareStatement("select color from color");
+			rs = pstmt.executeQuery();
+
+			while (rs.next())
+				list.add(rs.getString(1));
+
+		} catch (SQLException e) {
+			System.err.println("[getColor] sql error " + e.getMessage());
+		} 
+
+		return list;
+	}
+	
+	public ArrayList<String> getTrans() {
+		ArrayList<String> list = new ArrayList<String>();
+		ResultSet rs = null;
+
+		try {
+			conn = dataSrc.getConnection();
+			pstmt = conn.prepareStatement("select transmission from transmission");
+			rs = pstmt.executeQuery();
+
+			while (rs.next())
+				list.add(rs.getString(1));
+
+		} catch (SQLException e) {
+			System.err.println("[getTrans] sql error " + e.getMessage());
+		} 
+
+		return list;
+	}
+	
+	public String getCarnumber(String carnum,int flag) {
+		ArrayList<String> list = new ArrayList<String>();
+		ResultSet rs = null;
+
+		try {
+			pstmt = conn.prepareStatement("select Car_number from vehicle");
+			rs = pstmt.executeQuery();
+
+			while (rs.next())
+				list.add(rs.getString(1));
+
+		} catch (SQLException e) {
+			System.err.println("[getCarnumber] sql error " + e.getMessage());
+		} finally {
+			try {
+				rs.close();
+				pstmt.close();
+			} catch (SQLException e) {
+				System.err.println("[getCarnumber] sql error " + e.getMessage());
+			}
+		}
+
+		for (int i = 0; i < list.size(); i++) {
+			if (flag == 0) {
+				if (carnum.equals(list.get(i))) {
+					return null;
+				}
+			} else if (flag == 1) {
+				if (carnum.equals(list.get(i))) {
+					return carnum;
+				}
+			}
+		}
+
+		if (flag == 0)
+			return carnum;
+		else {
+			return null;
+		}
+	}
+	public boolean validCheck(String src, String regExp) {
+		return src.matches(regExp);
+	}
 }
